034d5d9181c7e20b5c3f1a7c951302355c5252b5
ntguilty <budnicki.piotr@gmail.com>
2020-08-05 01:38:01 +0200

    added flags and new logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// Add flag -addr to give http network address
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/note", showNote)
 	mux.HandleFunc("/note/create", createNote)
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+
+	//Create a server to serve files from "./ui/static/", register a file server as a handle for "/static/" and strip to match paths.
+	fileServerStatic := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServerStatic))
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	errorLog.Fatal(err)
 }
